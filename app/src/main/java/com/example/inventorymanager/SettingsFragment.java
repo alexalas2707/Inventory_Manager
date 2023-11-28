515d21b1ea1c13b51b61d541ecf146f791574d96
alexalas2707 <alexalas2707@gmail.com>
2023-11-28 13:21:43 -0800

    Everything except search in Dashboard working

--- app/src/main/java/com/example/inventorymanager/SettingsFragment.java
+++ app/src/main/java/com/example/inventorymanager/SettingsFragment.java
@@ -11,11 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class SettingsFragment extends Fragment{
+public class SettingsFragment extends Fragment {
 
     private EditText thresholdNumberEditText;
+    private SeekBar seekBarThreshold;
+
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -26,10 +29,40 @@ public class SettingsFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         thresholdNumberEditText = view.findViewById(R.id.thresholdNumber);
+        seekBarThreshold = view.findViewById(R.id.seekBar);
         Button saveButton = view.findViewById(R.id.saveThresholdButton);
+
+        // Initialize SeekBar and EditText with current threshold value
+        int currentThreshold = getCurrentThreshold();
+        seekBarThreshold.setMax(50); // Set max value for SeekBar
+        seekBarThreshold.setProgress(currentThreshold);
+        thresholdNumberEditText.setText(String.valueOf(currentThreshold));
+
+        // Update EditText when SeekBar value changes
+        seekBarThreshold.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                thresholdNumberEditText.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Optional method
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Optional method
+            }
+        });
 
         saveButton.setOnClickListener(v -> saveThresholdValue());
         return view;
+    }
+
+    private int getCurrentThreshold() {
+        SharedPreferences prefs = getActivity().getSharedPreferences("AppSettingsPrefs", Context.MODE_PRIVATE);
+        return prefs.getInt("ThresholdValue", 1); // Default to 1 if not set
     }
 
     private void saveThresholdValue() {
@@ -40,5 +73,4 @@ public class SettingsFragment extends Fragment{
         editor.apply();
         Toast.makeText(getActivity(), "Threshold saved successfully", Toast.LENGTH_SHORT).show();
     }
-
 }
